a0ce283f11f1df73673c6167317a1e9a1faa470b
David F <Skilliciousboy@icloud.com>
2021-01-05 13:23:57 +0000

    update

--- electron/main.js
+++ electron/main.js
@@ -1,20 +1,28 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const {app, BrowserWindow} = require('electron');
+const isDev = require('electron-is-dev');
+const path = require('path');
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth: 700,
+    minHeight: 500,
+    titleBarStyle: "hidden",
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js')
     }
   })
 
+  mainWindow.setMenuBarVisibility(false);
+
   // and load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000')
+  mainWindow.loadURL(
+    isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
